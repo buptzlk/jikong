5f552bbfa0bde2bf3fcaef8e0ca3bdadc6e3a8a0
youen123 <youlujie123@163.com>
2019-04-17 20:53:19 +0800

    add: 学习页面接口对接

--- pages/study/list.js
+++ pages/study/list.js
@@ -1,79 +1,58 @@
 // pages/study/list.js
+const Study = require('../../service/study.js')
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    list: [
-      {
-        id: 1,
-        imageUrl: '/image/banner.png',
-        title: '标题一',
-        desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。'
-      },
-      {
-        id: 2,
-        imageUrl: '/image/banner.png',
-        title: '标题一',
-        desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。'
-      }
-    ]
+    list: [],
+    index: 1,
+    page_size: 10,
+    hasNextPage: 1
   },
-
+  getList: function() {
+    if (this.data.hasNextPage != 1 || this.loading) {
+      return;
+    }
+    this.loading = true;
+    Study.getStudyList({
+      index: this.data.index,
+      page_size: this.data.page_size
+    }).then((data) => {
+      this.setData({
+        list: this.data.list.concat(data.newsInfo),
+        index: data.page.index,
+        hasNextPage: data.page.hasNextPage
+      })
+    }).catch((e) => {
+      console.log(e);
+      wx.showToast({
+        icon: 'none',
+        title: '获取学习资料失败',
+      })
+    }).then(() => {
+      this.loading = false;
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    this.getList()
   },
-
   /**
-   * 生命周期函数--监听页面初次渲染完成
+   * 页面上拉触底事件的处理函数
    */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  onReachBottom: function() {
+    this.getList()
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })

659d9920041747cd7f4915b825f6d9723f931440
youen123 <youlujie123@163.com>
2019-04-19 15:47:03 +0800

    add: 物资借用进度页接口对接

--- pages/material/process.js
+++ pages/material/process.js
@@ -1,26 +1,20 @@
 // pages/material/process.js
+const Material = require('../../service/material.js')
+const { showErrMsg, formatTime } = require('../../utils/util.js')
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    list: [
-      {
-        name: '防辐射服',
-        num: 2,
-        time: '2019-04-01 12:12:12',
-        statusText: '未审批'
-      },
-      {
-        name: '防辐射服',
-        num: 2,
-        time: '2019-04-01 12:12:12',
-        statusText: '未审批'
-      },
-    ],
+    list: [],
+    adminList: [],
     status: ['全部', '未审批', '审批通过', '已借用','拒绝'],
-    statusIndex: 0
+    statusIndex: 0,
+    index: 0,
+    page_size: 10,
+    hasNextPage: 1,
   },
 
   bindStatusChange: function (e) {
@@ -28,12 +22,51 @@ Page({
       statusIndex: e.detail.value
     })
   },
-
+  bindPickerChange: function(e) {
+    let goods_id = e.target.dataset.id
+    let admin_id = this.data.adminList[e.detail.value].id
+    console.log(goods_id, admin_id);
+  },
+  getList: function () {
+    if (this.data.hasNextPage != 1 || this.loading) {
+      return;
+    }
+    this.loading = true;
+    Material.getBorrowList({
+      index: this.data.index,
+      page_size: this.data.page_size
+    }).then((data) => {
+      this.setData({
+        list: this.data.list.concat(data.goodsInfo),
+        index: data.page.index,
+        hasNextPage: data.page.hasNextPage
+      })
+    }).catch((e) => {
+      console.log(e);
+      wx.showToast({
+        icon: 'none',
+        title: '获取物资信息失败'
+      })
+    }).then(() => {
+      this.loading = false;
+    })
+  },
+  getAdminList: function(){
+    Material.getAdminList().then((data) => {
+      this.setData({
+        adminList: data
+      })
+    }).catch((e) => {
+      console.log(e)
+      showErrMsg(e || '获取管理员列表失败')
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getList()
+    this.getAdminList();
   },
 
   /**
@@ -75,7 +108,7 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    this.getList();
   },
 
   /**

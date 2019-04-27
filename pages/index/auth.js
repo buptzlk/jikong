07835ed1dd396b59be85539ad516cf1407b47097
youen123 <youlujie123@163.com>
2019-04-27 23:03:33 +0800

    fix: 解决需多次授权的问题

--- pages/index/auth.js
+++ pages/index/auth.js
@@ -10,7 +10,20 @@ Page({
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
+  onReady() {
+  },
   onLoad() {
+    if (app.globalData.openid && app.globalData.isBind) {
+      wx.switchTab({
+        url: 'index',
+      })
+    } else if (app.globalData.openid) {
+      wx.redirectTo({
+        url: '../login/login',
+      })
+    } else {
+      this.login();
+    }  
   },
   bindGetUserInfo(e) {
     if (e.detail.userInfo) {
@@ -21,25 +34,40 @@ Page({
         userInfo: e.detail.userInfo,
         hasUserInfo: true
       })
-      User.login({
-        code: app.globalData.code,
-        iv: encodeURIComponent(app.globalData.iv),
-        encryptedData: encodeURIComponent(app.globalData.encryptedData)
-      }).then((data) => {
-        app.globalData.openid = data.openid;
-        wx.switchTab({
-          url: 'index',
-        })
-      }).catch((e) => {
-        if (e && e.status_code == 4003) {
-          app.globalData.openid = e.data.openid;
-          wx.redirectTo({
-            url: '../login/login',
-          })
-          return;
-        }
-        showErrMsg(e.message || '登录失败');
-      })
+      this.login()
     }
+  },
+  login() {
+    console.log(app.globalData);
+    if (!app.globalData.code || !app.globalData.iv || !app.globalData.encryptedData || app.globalData.openid || this.loadding) {
+      return;
+    }
+    console.log('try login');
+    this.loadding = true
+    User.login({
+      code: app.globalData.code,
+      iv: encodeURIComponent(app.globalData.iv),
+      encryptedData: encodeURIComponent(app.globalData.encryptedData)
+    }).then((data) => {
+      app.globalData.openid = data.openid;
+      app.globalData.isBind = true;
+      wx.switchTab({
+        url: 'index',
+      })
+      this.loadding = false;
+    }).catch((e) => {
+      this.loadding = false;
+      if (e && e.status_code == 4003) {
+        app.globalData.openid = e.data.openid;
+        app.globalData.isBind = false;
+        wx.redirectTo({
+          url: '../login/login',
+        })
+        return;
+      }
+
+      showErrMsg(e.message || '登录失败');
+    })
   }
 })
+
